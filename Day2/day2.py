3b68e45c7a02744550123b825dcec4bca652f3a9
misstonbon <plusdeltah@gmail.com>
2018-12-01 22:16:19 -0800

    day 2 part 1

---
+++ Day2/day2.py
@@ -0,0 +1,28 @@
+import collections
+
+with open('daytwo.txt', 'r') as file:
+    content = file.readlines()
+    lines = []
+    for line in content:
+        lines.append(line[:-1])
+
+counts = {}
+for line in lines:
+    counts[line] = collections.Counter(line)
+
+twice = set()
+three_times = set()
+
+for key in counts:
+    for letter, count in counts[key].items():
+        if count == 2 and key not in twice:
+            twice.add(key)
+        if count == 3  and key not in three_times:
+            three_times.add(key)
+        # print('%s: %7d' % (letter, count))
+
+checksum = len(twice) * len(three_times)
+print( checksum) #7470
+
+print(len(twice))
+print(len(three_times))
